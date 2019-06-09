f1b2689af39c83618d71120417bcd2465349c8f3
Mauricio Mercado <mau@maumercado.com>
2019-06-09 14:34:34 -0700

    fix(sra): runtimegenerator error, required pollyfill

--- bin/sra.js
+++ bin/sra.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+require("@babel/polyfill");
+
 const { exec, execSync, spawnSync } = require('child_process');
 const { promisify } = require('util');
 const chalk = require('chalk');
